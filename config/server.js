58a52727b74d570888fda87424fae67ffc56e031
Kasper Siig <kasper@siig.tech>
2021-07-13 11:09:23 +0200

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '77ab5f45d0c73514a1a194dd60a99141'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6809966f-fb9e-4427-924e-b0955e9963c6'
+};
